83a1bebcc25397bf85169860f80545aafb73ec1c
Ricardo Bravo <ricardobravo@outlook.com>
2017-01-16 18:18:26 -0500

    wip(SaleByStoreAndYear): dummy data added

--- app/src/main/java/com/rba/chartdemo/salebystoreandyear/SaleByStoreAndYearActivity.java
+++ app/src/main/java/com/rba/chartdemo/salebystoreandyear/SaleByStoreAndYearActivity.java
@@ -1,18 +1,30 @@
 package com.rba.chartdemo.salebystoreandyear;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.rba.chartdemo.R;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class SaleByStoreAndYearActivity extends AppCompatActivity implements SaleByStoreAndYearView {
+public class SaleByStoreAndYearActivity extends AppCompatActivity implements
+        SaleByStoreAndYearView {
 
     @BindView(R.id.lchSale) LineChart lchSale;
     @BindView(R.id.toolbar) Toolbar toolbar;
@@ -42,6 +54,68 @@ public class SaleByStoreAndYearActivity extends AppCompatActivity implements Sal
                 finish();
             }
         });
+
+
+
+        lchSale.setDrawGridBackground(false);
+        lchSale.setTouchEnabled(true);
+        lchSale.getDescription().setEnabled(false);
+
+        XAxis xAxis = lchSale.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        xAxis.setDrawAxisLine(true);
+
+        YAxis leftAxis = lchSale.getAxisLeft();
+        leftAxis.setLabelCount(5, false);
+        leftAxis.setAxisMinimum(0f);
+
+        YAxis rightAxis = lchSale.getAxisRight();
+        rightAxis.setLabelCount(5, false);
+        rightAxis.setDrawGridLines(false);
+        rightAxis.setAxisMinimum(0f);
+
+        lchSale.animateX(750);
+
+
+
+        ArrayList<Entry> e1 = new ArrayList<Entry>();
+
+        for (int i = 0; i < 12; i++) {
+            e1.add(new Entry(i, (int) (Math.random() * 65) + 40));
+        }
+
+        LineDataSet d1 = new LineDataSet(e1, "New DataSet , (1)");
+        d1.setLineWidth(2.5f);
+        d1.setCircleRadius(4.5f);
+        d1.setHighLightColor(Color.rgb(244, 117, 117));
+        d1.setDrawValues(false);
+
+        ArrayList<Entry> e2 = new ArrayList<Entry>();
+
+        for (int i = 0; i < 12; i++) {
+            e2.add(new Entry(i, e1.get(i).getY() - 30));
+        }
+
+        LineDataSet d2 = new LineDataSet(e2, "New DataSet , (2)");
+        d2.setLineWidth(2.5f);
+        d2.setCircleRadius(4.5f);
+        d2.setHighLightColor(Color.rgb(244, 117, 117));
+        d2.setColor(ColorTemplate.VORDIPLOM_COLORS[0]);
+        d2.setCircleColor(ColorTemplate.VORDIPLOM_COLORS[0]);
+        d2.setDrawValues(false);
+
+        ArrayList<ILineDataSet> sets = new ArrayList<ILineDataSet>();
+        sets.add(d1);
+        sets.add(d2);
+
+        LineData cd = new LineData(sets);
+
+        lchSale.setData(cd);
+        lchSale.invalidate();
+
+
+
     }
 
     @Override
