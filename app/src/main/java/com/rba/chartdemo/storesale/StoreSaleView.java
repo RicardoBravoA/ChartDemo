1e92e0d71c0ec551ba533b3e192c437bf9b167d6
Ricardo Bravo <ricardobravo@outlook.com>
2017-01-24 11:23:29 -0500

    feat(StoreSale): view added

---
+++ app/src/main/java/com/rba/chartdemo/storesale/StoreSaleView.java
@@ -0,0 +1,23 @@
+package com.rba.chartdemo.storesale;
+
+import com.rba.chartdemo.model.response.StoreResponse;
+import com.rba.chartdemo.model.response.StoreYearResponse;
+
+/**
+ * Created by Ricardo Bravo on 24/01/17.
+ */
+
+public interface StoreSaleView {
+
+    void init();
+
+    void showErrorMessage(String message);
+
+    void showStoreYear(StoreYearResponse storeYearResponse);
+
+    void showStore(StoreResponse storeResponse);
+
+    void showErrorYear(String message);
+
+
+}
